a051854b6a53c2bcd45b414527a5a3a057c4cb12
Andrew Regan <aregan@gmail.com>
2014-02-17 17:01:49 +0000

    Handle display of versions

--- providers/local.maven.java/project/src/main/java/com/andrewregan/kodomondo/LocalMavenServer.java
+++ providers/local.maven.java/project/src/main/java/com/andrewregan/kodomondo/LocalMavenServer.java
@@ -5,6 +5,8 @@ import java.io.FileFilter;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.jar.JarEntry;
@@ -13,6 +15,7 @@ import java.util.jar.JarFile;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Ordering;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
@@ -59,6 +62,7 @@ public class LocalMavenServer
 
 			if (f.isDirectory()) {
 				List<DirEntry> dirsList = Lists.newArrayList();
+				Collection<String> versions = Lists.newArrayList();
 
 				File[] files = f.listFiles( new FileFilter() {
 
@@ -67,10 +71,31 @@ public class LocalMavenServer
 					}} );
 
 				for ( File each : files) {
-					dirsList.add( new DirEntry( "/" + each.getName() ) );
+					if (each.isDirectory()) {
+						if (Character.isDigit( each.getName().charAt(0) )) {
+							versions.add( each.getName() );
+						}
+						else {
+							dirsList.add( new DirEntry( "/" + each.getName() ) );
+						}
+					}
+					else {
+						if (!each.getName().endsWith(".jar")) {
+							continue;
+						}
+						System.out.println( each.getName() );
+					}
 				}
 
-				final String output = MAPPER.writeValueAsString( new DirResponse( f.getAbsolutePath(), dirsList) );
+				final String output;
+
+				if (!versions.isEmpty()) {
+					String highest = Ordering.from( new VersionComparator() ).max(versions);
+					output = MAPPER.writeValueAsString( new VersionResponse( f.getAbsolutePath(), highest) );
+				}
+				else {
+					output = MAPPER.writeValueAsString( new DirResponse( f.getAbsolutePath(), dirsList) );
+				}
 
 				t.sendResponseHeaders(200, output.length());
 				OutputStream os = t.getResponseBody();
@@ -95,8 +120,6 @@ public class LocalMavenServer
 							continue;
 						}
 	
-	//					System.out.println( "Class: " + eachName);
-	
 						classesList.add( new ClassEntry( eachName.substring( 0, eachName.length() - 6) .replace('/', '.')) );
 	//					InputStream theStream = jf.getInputStream(eachEntry);
 	//					ClassReader cr = new ClassReader( Files.toByteArray(f) );
@@ -119,6 +142,21 @@ public class LocalMavenServer
 		}
 	}
 
+	private static class VersionResponse {
+		final String name;
+		final String version;
+
+		public VersionResponse( String inName, String inVersion) {
+			this.name = inName;
+			this.version = inVersion;
+		}
+
+		@JsonProperty("version")
+		public String geVersion() {
+			return version;
+		}
+	}
+
 	private static class DirResponse {
 		final String name;
 		final List<DirEntry> dirs;
@@ -127,11 +165,6 @@ public class LocalMavenServer
 			this.name = inName;
 			this.dirs = inClasses;
 		}
-
-//		@JsonProperty("name")
-//		public String getJarName() {
-//			return name;
-//		}
 
 		@JsonProperty("dirs")
 		public List<DirEntry> getClasses() {
@@ -182,6 +215,145 @@ public class LocalMavenServer
 		@JsonProperty("class")
 		public String getClassName() {
 			return className;
+		}
+	}
+
+	// From http://stackoverflow.com/a/10034633/954442
+	private static class VersionComparator implements Comparator<String> {
+
+		public boolean equals(String o1, String o2) {
+			return compare(o1, o2) == 0;
+		}
+
+		public int compare(String version1, String version2) {
+
+			VersionTokenizer tokenizer1 = new VersionTokenizer(version1);
+			VersionTokenizer tokenizer2 = new VersionTokenizer(version2);
+
+			int number1 = 0, number2 = 0;
+			String suffix1 = "", suffix2 = "";
+
+			while (tokenizer1.MoveNext()) {
+				if (!tokenizer2.MoveNext()) {
+					do {
+						number1 = tokenizer1.getNumber();
+						suffix1 = tokenizer1.getSuffix();
+						if (number1 != 0 || suffix1.length() != 0) {
+							// Version one is longer than number two, and non-zero
+							return 1;
+						}
+					}
+					while (tokenizer1.MoveNext());
+
+					// Version one is longer than version two, but zero
+					return 0;
+				}
+
+				number1 = tokenizer1.getNumber();
+				suffix1 = tokenizer1.getSuffix();
+				number2 = tokenizer2.getNumber();
+				suffix2 = tokenizer2.getSuffix();
+
+				if (number1 < number2) {
+					// Number one is less than number two
+					return -1;
+				}
+				if (number1 > number2) {
+					// Number one is greater than number two
+					return 1;
+				}
+
+				boolean empty1 = suffix1.length() == 0;
+				boolean empty2 = suffix2.length() == 0;
+
+				if (empty1 && empty2) continue; // No suffixes
+				if (empty1) return 1; // First suffix is empty (1.2 > 1.2b)
+				if (empty2) return -1; // Second suffix is empty (1.2a < 1.2)
+
+				// Lexical comparison of suffixes
+				int result = suffix1.compareTo(suffix2);
+				if (result != 0) return result;
+
+			}
+			if (tokenizer2.MoveNext()) {
+				do {
+					number2 = tokenizer2.getNumber();
+					suffix2 = tokenizer2.getSuffix();
+					if (number2 != 0 || suffix2.length() != 0) {
+						// Version one is longer than version two, and non-zero
+						return -1;
+					}
+				}
+				while (tokenizer2.MoveNext());
+
+				// Version two is longer than version one, but zero
+				return 0;
+			}
+			return 0;
+		}
+	}
+
+	// From http://stackoverflow.com/a/10034633/954442
+	static class VersionTokenizer {
+		private final String _versionString;
+		private final int _length;
+
+		private int _position;
+		private int _number;
+		private String _suffix;
+		private boolean _hasValue;
+
+		public int getNumber() {
+			return _number;
+		}
+
+		public String getSuffix() {
+			return _suffix;
+		}
+
+		public boolean hasValue() {
+			return _hasValue;
+		}
+
+		public VersionTokenizer(String versionString) {
+			if (versionString == null)
+				throw new IllegalArgumentException("versionString is null");
+
+			_versionString = versionString;
+			_length = versionString.length();
+		}
+
+		public boolean MoveNext() {
+			_number = 0;
+			_suffix = "";
+			_hasValue = false;
+
+			// No more characters
+			if (_position >= _length)
+				return false;
+
+			_hasValue = true;
+
+			while (_position < _length) {
+				char c = _versionString.charAt(_position);
+				if (c < '0' || c > '9') break;
+				_number = _number * 10 + (c - '0');
+				_position++;
+			}
+
+			int suffixStart = _position;
+
+			while (_position < _length) {
+				char c = _versionString.charAt(_position);
+				if (c == '.') break;
+				_position++;
+			}
+
+			_suffix = _versionString.substring(suffixStart, _position);
+
+			if (_position < _length) _position++;
+
+			return true;
 		}
 	}
 }
