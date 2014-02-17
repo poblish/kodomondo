1cc7bedaa190438abc3018e6f79d2bc8b40e13cb
Andrew Regan <aregan@gmail.com>
2014-02-17 23:40:02 +0000

    Ensure spider always gets DB

--- chrome/project/background/background.js
+++ chrome/project/background/background.js
@@ -10,55 +10,6 @@ _gaq.push(['_trackPageview']);
 
 ////////////////////////////////
 
-pullDataSources();
-
-chrome.alarms.create('Pull DataSources', {periodInMinutes: 5}); // every n mins
-chrome.alarms.onAlarm.addListener( function(alarm) {
-  pullDataSources();
-})
-
-function pullDataSources() {
-	console.log('pullDataSources:', new Date());
-	fetchDir('http://localhost:2000');
-}
-
-function fetchDir(inDir) {
-	$.get(inDir)
-			.error( function(xhr) { /* Anything? */ } )
-			.success( function(obj) {
-					try {
-							if ( obj.dirs != null) {
-									for ( i = 0; i < obj.dirs.length; i++) {
-										fetchDir( inDir + obj.dirs[i].dir );
-									}
-							}
-							else if ( obj.version != null) {
-									fetchJar( inDir + '/' + obj.version);
-							}
-					} catch (e) { /* Just ignore */ }
-			});
-}
-
-function fetchJar(inUrl) {
-	$.get(inUrl)
-			.error( function(xhr) { /* Anything? */ } )
-			.success( function(obj) {
-					try {
-							if ( obj.jar != null) {
-									console.log( obj.classes.length + ' classes for: ' + obj.jar);
-									for ( i = 0; i < obj.classes.length; i++) {
-										recordClass( obj.classes[i].class );
-									}
-							}
-					} catch (e) { /* Just ignore */ }
-			});
-}
-
-function recordClass(inClazz) {
-}
-
-////////////////////////////////
-
 // See: http://www.w3.org/TR/IndexedDB/
 // Pull from http://localhost:2000/org/apache/lucene/lucene-core/4.3.1/lucene-core-4.3.1.jar
 
@@ -80,24 +31,75 @@ openReq.onupgradeneeded = function(evt) {
 };
 
 openReq.onsuccess = function(evt) {
-  var db = evt.target.result;
-  var tx = db.transaction("books", "readonly");
-  var store = tx.objectStore("books");
-  var index = store.index("by_title");
+	var db = evt.target.result;
+	var tx = db.transaction("books", "readonly");
+	var store = tx.objectStore("books");
+	var index = store.index("by_title");
 
-  var request = index.get("Bedrock Nights");
-  request.onsuccess = function() {
-    var matching = request.result;
-    if (matching !== undefined) {
-      // A match was found.
-      console.log('REPORT', matching.isbn, matching.title, matching.author);
-    } else {
-      // No match was found.
-      console.log('REPORT', null);
-    }
-  };
+	var request = index.get("Bedrock Nights");
+	request.onsuccess = function() {
+		var matching = request.result;
+		if (matching !== undefined) {
+			// A match was found.
+			console.log('REPORT', matching.isbn, matching.title, matching.author);
+		} else {
+			// No match was found.
+			console.log('REPORT', null);
+		}
+	};
+
+	// Now we have IndexedDB, pull DataSources
+
+	pullDataSources(db);
+
+	chrome.alarms.create('Pull DataSources', {periodInMinutes: 5}); // every n mins
+	chrome.alarms.onAlarm.addListener( function(alarm) {
+		pullDataSources(db);
+	})
+
+	function pullDataSources(inDB) {
+		console.log('pullDataSources:', new Date());
+		fetchDir('http://localhost:2000', inDB);
+	}
 };
 
 openReq.onerror = function() {
   report(openReq.error);
+}
+
+function fetchDir(inDir, inDB) {
+	console.log('DB: ', inDB);
+	$.get(inDir)
+			.error( function(xhr) { /* Anything? */ } )
+			.success( function(obj) {
+					try {
+							if ( obj.dirs != null) {
+									for ( i = 0; i < obj.dirs.length; i++) {
+										fetchDir( inDir + obj.dirs[i].dir, inDB);
+									}
+							}
+							else if ( obj.version != null) {
+									fetchJar( inDir + '/' + obj.version, inDB);
+							}
+					} catch (e) { /* Just ignore */ }
+			});
+}
+
+function fetchJar(inUrl, inDB) {
+	console.log('Jar DB: ', inDB);
+	$.get(inUrl)
+			.error( function(xhr) { /* Anything? */ } )
+			.success( function(obj) {
+					try {
+							if ( obj.jar != null) {
+									console.log( obj.classes.length + ' classes for: ' + obj.jar);
+									for ( i = 0; i < obj.classes.length; i++) {
+										recordClass( obj.classes[i].class );
+									}
+							}
+					} catch (e) { /* Just ignore */ }
+			});
+}
+
+function recordClass(inClazz, inDB) {
 }
