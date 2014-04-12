d592921b803c2caec553e08e953aeda5a0383e4e
Andrew Regan <aregan@gmail.com>
2014-04-13 00:39:13 +0100

    Fix config location/install in Brew formula

--- providers/Formula/kodomondo.rb
+++ providers/Formula/kodomondo.rb
@@ -7,11 +7,17 @@ class Kodomondo < Formula
 
   resource 'startupScript' do
     url 'https://raw.githubusercontent.com/poblish/kodomondo/master/providers/bin/kodomondo.sh'
-    sha1 'd8ebcff7dda5ff0b418a4685dbe99e0148ffc957'
+    sha1 '35bab5dbc5fb3457b07e323e1281d15126d603c3'
+  end
+
+  resource 'configScript' do
+    url 'https://raw.githubusercontent.com/poblish/kodomondo/master/providers/config/ds.yaml'
+    sha1 '6e77a1deaaf6cab5e144164319de64b928b6164c'
   end
 
   def install
     resource("startupScript").stage { bin.install "kodomondo.sh" }  # FIXME Need to install 'kodomondo'
+    resource("configScript").stage { (prefix/"config").install "ds.yaml" }
     libexec.install Dir['*']
     # bin.install_symlink "#{bin}/kodomondo.sh" => "kodomondo"
   end
