28e5d35fe49975d50a184e20ca315bcb8548b59f
Andrew Regan <aregan@gmail.com>
2014-05-04 16:16:00 +0100

    Fix tests

--- server/src/test/java/com/andrewregan/kodomondo/tasks/JavaDocDownloadTaskTest.java
+++ server/src/test/java/com/andrewregan/kodomondo/tasks/JavaDocDownloadTaskTest.java
@@ -2,20 +2,30 @@ package com.andrewregan.kodomondo.tasks;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.io.File;
 
 import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
 
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.andrewregan.kodomondo.LocalMavenConfig;
 import com.andrewregan.kodomondo.es.EsUtils;
+import com.andrewregan.kodomondo.fs.TestFileObject;
+import com.andrewregan.kodomondo.fs.api.IFileObject;
 import com.andrewregan.kodomondo.fs.api.IFileSystem;
+import com.andrewregan.kodomondo.maven.util.ArtifactDesc;
 
 import dagger.Module;
 import dagger.ObjectGraph;
+import dagger.Provides;
 
 /**
  * 
@@ -37,34 +47,44 @@ public class JavaDocDownloadTaskTest {
 
 	@Test
 	public void testTask() {
-		assertThat( "JavaDoc should not already exist", new File("/Users/andrewregan/.m2/repository/com/google/guava/guava/16.0.1/guava-16.0.1-javadoc.jar").exists(), is(false));
+		assertThat( "JavaDoc should not already exist", new File("/usr/blah/com/google/guava/guava/16.0.1/guava-16.0.1-javadoc.jar").exists(), is(false));
 
-		final JavaDocDownloadTask task = docsDownloaderFactory.create( fs.resolveFile("/Users/andrewregan/.m2/repository/com/google/guava/guava/16.0.1/guava-16.0.1.jar") );
+		final JavaDocDownloadTask task = docsDownloaderFactory.create( fs.resolveFile("/usr/blah/com/google/guava/guava/16.0.1/guava-16.0.1.jar") );
 
 		esEnv.waitForStatus();
 
 		assertThat( task.call(), is(0));
 
 		// Check file deleted
-		assertThat( "File not deleted", new File("/Users/andrewregan/.m2/repository/com/google/guava/guava/16.0.1/guava-16.0.1-javadoc.jar").exists(), is(false));
-		assertThat( "File not deleted", new File("/Users/andrewregan/.m2/repository/com/google/guava/guava/16.0.1/guava-16.0.1-javadoc.jar.sha1").exists(), is(false));
+		assertThat( "File not deleted", new File("/usr/blah/com/google/guava/guava/16.0.1/guava-16.0.1-javadoc.jar").exists(), is(false));
+		assertThat( "File not deleted", new File("/usr/blah/com/google/guava/guava/16.0.1/guava-16.0.1-javadoc.jar.sha1").exists(), is(false));
 	}
 
 	@Module( includes=LocalMavenConfig.class, overrides=true, injects=JavaDocDownloadTaskTest.class)
 	static class TestModule {
 
-//		@Provides
-//		@Singleton
-//		IFileSystem provideFileSystemManager() {
-//			return new OurTestFileSystem();
-//		}
-	}
+		@Provides @Named("mvnRoot")
+		IFileObject provideMavenRoot(IFileSystem inFS) {
+			return new TestFileObject( getFS(), "/usr/blah");
+		}
 
-//	private static class OurTestFileSystem extends TestFileSystem {
-//
-//		@Override
-//		public ArtifactDesc toArtifact( IFileObject file) {
-//			return ArtifactDesc.forFile( /* FIXME */ new File(((TestFileObject) file).getAbsolutePath()) );
-//		}
-//	}
+		@Provides @Singleton
+		IFileSystem provideFileSystemManager() {
+			return getFS();
+		}
+
+		private IFileSystem getFS() {
+			final IFileSystem ifs = mock( IFileSystem.class );
+
+			when (ifs.resolveFile( anyString() ))
+				.thenReturn( new TestFileObject( ifs, "/usr/blah/com/google/guava/guava/16.0.1/guava-16.0.1.jar") );
+
+			when (ifs.resolveFile( any( IFileObject.class ), anyString() ))
+				.thenReturn( new TestFileObject( ifs, "/usr/blah/com/google/guava/guava/16.0.1", true, new IFileObject[]{}) );
+
+			when(ifs.toArtifact( any( IFileObject.class ) )).thenReturn( new ArtifactDesc("com.google.guava", "guava", "16.0.1") );
+
+			return ifs;
+		}
+	}
 }
--- server/src/test/java/com/andrewregan/kodomondo/tasks/SourceDownloadTaskTest.java
+++ server/src/test/java/com/andrewregan/kodomondo/tasks/SourceDownloadTaskTest.java
@@ -2,22 +2,30 @@ package com.andrewregan.kodomondo.tasks;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.io.File;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.inject.Singleton;
 
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.andrewregan.kodomondo.LocalMavenConfig;
 import com.andrewregan.kodomondo.es.EsUtils;
+import com.andrewregan.kodomondo.fs.TestFileObject;
 import com.andrewregan.kodomondo.fs.api.IFileObject;
 import com.andrewregan.kodomondo.fs.api.IFileSystem;
+import com.andrewregan.kodomondo.maven.util.ArtifactDesc;
 
 import dagger.Module;
 import dagger.ObjectGraph;
+import dagger.Provides;
 
 /**
  * 
@@ -27,9 +35,6 @@ import dagger.ObjectGraph;
  *
  */
 public class SourceDownloadTaskTest {
-
-	@Named("mvnRoot")
-	@Inject IFileObject mvnRoot;
 
 	@Inject EsUtils esEnv;
 	@Inject IFileSystem fs;
@@ -42,19 +47,44 @@ public class SourceDownloadTaskTest {
 
 	@Test
 	public void testTask() {
-		assertThat( "Source should not already exist", new File( mvnRoot.getPath(), "/com/google/guava/guava/16.0.1/guava-16.0.1-sources.jar").exists(), is(false));
+		assertThat( "Source should not already exist", new File("/usr/blah/com/google/guava/guava/16.0.1/guava-16.0.1-sources.jar").exists(), is(false));
 
-		final SourceDownloadTask task = srcDownloaderFactory.create( fs.resolveFile( mvnRoot.getPath() + "/com/google/guava/guava/16.0.1/guava-16.0.1.jar") );
+		final SourceDownloadTask task = srcDownloaderFactory.create( fs.resolveFile("/usr/blah/com/google/guava/guava/16.0.1/guava-16.0.1.jar") );
 
 		esEnv.waitForStatus();
 
 		assertThat( task.call(), is(0));
 
 		// Check file deleted
-		assertThat( "File not deleted", new File( mvnRoot.getPath(), "/com/google/guava/guava/16.0.1/guava-16.0.1-sources.jar").exists(), is(false));
-		assertThat( "File not deleted", new File( mvnRoot.getPath(), "/com/google/guava/guava/16.0.1/guava-16.0.1-sources.jar.sha1").exists(), is(false));
+		assertThat( "File not deleted", new File("/usr/blah/com/google/guava/guava/16.0.1/guava-16.0.1-sources.jar").exists(), is(false));
+		assertThat( "File not deleted", new File("/usr/blah/com/google/guava/guava/16.0.1/guava-16.0.1-sources.jar.sha1").exists(), is(false));
 	}
 
 	@Module( includes=LocalMavenConfig.class, overrides=true, injects=SourceDownloadTaskTest.class)
-	static class TestModule {}
+	static class TestModule {
+
+		@Provides @Named("mvnRoot")
+		IFileObject provideMavenRoot(IFileSystem inFS) {
+			return new TestFileObject( getFS(), "/usr/blah");
+		}
+
+		@Provides @Singleton
+		IFileSystem provideFileSystemManager() {
+			return getFS();
+		}
+
+		private IFileSystem getFS() {
+			final IFileSystem ifs = mock( IFileSystem.class );
+
+			when (ifs.resolveFile( anyString() ))
+				.thenReturn( new TestFileObject( ifs, "/usr/blah/com/google/guava/guava/16.0.1/guava-16.0.1.jar") );
+
+			when (ifs.resolveFile( any( IFileObject.class ), anyString() ))
+				.thenReturn( new TestFileObject( ifs, "/usr/blah/com/google/guava/guava/16.0.1", true, new IFileObject[]{}) );
+
+			when(ifs.toArtifact( any( IFileObject.class ) )).thenReturn( new ArtifactDesc("com.google.guava", "guava", "16.0.1") );
+
+			return ifs;
+		}
+	}
 }
