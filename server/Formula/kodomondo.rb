e8c4778c603dd9ef8f30959fd0b5a6bb72954292
Andrew Regan <aregan@gmail.com>
2014-05-06 01:37:31 +0100

    Fix formula locations

--- server/Formula/kodomondo.rb
+++ server/Formula/kodomondo.rb
@@ -6,17 +6,17 @@ class Kodomondo < Formula
   sha1 "8f403fc725e462708f7567216d085f5ffb3f71a9"
 
   resource 'startupScript' do
-    url 'https://raw.githubusercontent.com/poblish/kodomondo/master/providers/bin/kodomondo'
+    url 'https://raw.githubusercontent.com/poblish/kodomondo/master/server/bin/kodomondo'
     sha1 '35bab5dbc5fb3457b07e323e1281d15126d603c3'
   end
 
   resource 'startupScriptSh' do
-    url 'https://raw.githubusercontent.com/poblish/kodomondo/master/providers/bin/kodomondo.sh'
+    url 'https://raw.githubusercontent.com/poblish/kodomondo/master/server/bin/kodomondo.sh'
     sha1 '35bab5dbc5fb3457b07e323e1281d15126d603c3'
   end
 
   resource 'configScript' do
-    url 'https://raw.githubusercontent.com/poblish/kodomondo/master/providers/config/ds.yaml'
+    url 'https://raw.githubusercontent.com/poblish/kodomondo/master/server/config/ds.yaml'
     sha1 '6e77a1deaaf6cab5e144164319de64b928b6164c'
   end
 
